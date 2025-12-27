50fa6ff13b219b7d99f70c5ad6a4a361759dedcc
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2025-12-27 23:31:22 +0900

    Create tb_3_to_8_decoder.v

---
+++ 01_Combinational_Circuits/Mux_Decoder/tb_3_to_8_decoder.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer:      Sungyoon Jung
+// Create Date:   2024
+// Module Name:   tb_3_to_8_decoder.v
+// Target Module: 3_to_8_decoder.v
+//
+// Description:   
+//    Testbench for verifying the 3 x 8 decoder.
+//
+// Test Strategy:
+//   Apply random input to verify the 3 x 8 decoder.
+//////////////////////////////////////////////////////////////////////////////////
+
+module tb_decoder();
+
+  reg [2:0] s;
+  wire [7:0] d;
+
+  decoder decoder0(s, d);
+
+  initial begin
+      s = 3'b000; #15
+      s = 3'b001; #15
+      s = 3'b010; #15
+      s = 3'b011; #15
+      s = 3'b100; #15
+      s = 3'b101; #15 
+      s = 3'b110; #15 
+      s = 3'b111; #15
+      $finish;
+  end    
+  endmodule
