289e6de0d15278515e9f09cfb8d9f7ad2ae92dc1
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2025-12-27 23:17:26 +0900

    Update tb_four_bit_adder_struc.v

--- 01_Combinational_Circuits/Arithmetic_Logic/tb_four_bit_adder_struc.v
+++ 01_Combinational_Circuits/Arithmetic_Logic/tb_four_bit_adder_struc.v
@@ -14,18 +14,18 @@
 
 module tb_four_bit_adder();
 
-reg [3:0] x;
-reg [3:0] y;
-wire [3:0] s;
-wire c;
+    reg [3:0] x;
+    reg [3:0] y;
+    wire [3:0] s;
+    wire c;
 
 four_bit_adder_struc four_bit_adder0(x, y, s, c);
 
-initial begin
-    x = 4'b1101; y = 4'b0100; #15
-    x = 4'b0111; y = 4'b0010; #15
-    x = 4'b1001; y = 4'b1111; #15
-    x = 4'b1111; y = 4'b1001; #15
-    $finish;
+    initial begin
+        x = 4'b1101; y = 4'b0100; #15
+        x = 4'b0111; y = 4'b0010; #15
+        x = 4'b1001; y = 4'b1111; #15
+        x = 4'b1111; y = 4'b1001; #15
+        $finish;
     end
 endmodule
