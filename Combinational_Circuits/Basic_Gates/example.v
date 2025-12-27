dfc136c7696c75990933e2f24fdf0389b8fc56cc
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2025-12-27 22:45:24 +0900

    Create example.v

---
+++ Combinational_Circuits/Basic_Gates/example.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer:    Sungyoon Jung
+// Create Date: 2024
+// Module Name: example.v
+// Project:     Digital Logic Design Portfolio
+//
+// Description: 
+//    Basic AND, OR, NOT gate.
+//////////////////////////////////////////////////////////////////////////////////
+
+module example(a, b, c, e, f);
+
+  input a, b, c;
+  output e, f;
+  wire d;
+
+  and g1(d, a, b);
+  not g2(e, c);
+  or g3(f, d, e);
+
+endmodule
