3c5bd8775e5ae5a106aa973a36c8acd9dc7466b4
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2025-12-28 13:23:44 +0900

    Create counter.v

---
+++ 02_Sequential_Logic/02_4bit_Counter/counter.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer:    Sungyoon Jung
+// Create Date: 2024
+// Module Name: counter.v
+// Project:     Digital Logic Design Portfolio
+//
+// Description: 
+//    Make a 4 bit counter.
+//////////////////////////////////////////////////////////////////////////////////
+
+module counter(clk, reset, cnt_zero, cnt_one);
+
+  input clk, reset;
+  output reg [3:0] cnt_zero;
+  output reg [3:0] cnt_one;
+
+  always @ (posedge clk or negedge reset) begin
+      if(!reset) begin
+          cnt_zero <= 4'b0000;
+          cnt_one <= 4'b0000;
+      end
+      else begin
+          if (cnt_zero == 4'b1001) begin
+              cnt_zero <= 4'b0000;
+              cnt_one <= cnt_one + 4'b0001;
+          end
+          else if (cnt_one == 4'b0001 && cnt_zero == 4'b0101) begin
+              cnt_zero <= 4'b0000;
+              cnt_one <= 4'b0000;
+          end
+      else
+          cnt_zero <= cnt_zero + 4'b0001;            
+      end
+  end
+endmodule
