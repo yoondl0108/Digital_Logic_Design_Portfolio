c8aed38e7f0344bf73b5b36acdd2a5517bc03b37
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2026-01-12 11:17:16 +0900

    Create Memory_Logic.v

---
+++ 04_Memory_Design/02_Memory_Integration/rtl/Memory_Logic.v
@@ -0,0 +1,22 @@
+module memory_logic(clk, we, adrs, d_in, mem);
+
+  parameter
+  ADDR_WIDTH = 2, WORD_DEPTH = 4, WORD_WIDTH = 8;
+  
+  input        clk;
+  input        we;
+  input        [ADDR_WIDTH - 1 : 0] adrs;
+  input        [ADDR_WIDTH - 1 : 0] d_in;
+  output reg   [WORD_WIDTH - 1 : 0] mem;
+
+  reg [WORD_WIDTH - 1 : 0] memory [0 : WORD_DEPTH - 1];
+
+  always @ (posedge clk) begin
+    if(!we)
+      memorry[adrs] <= d_in;
+  end
+
+  always @ (posedge clk) begin
+    #1 mem <= memory[adrs];
+  end
+endmodule
