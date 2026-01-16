5082b160960c6d844aa890ce56a608da52aa5695
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2026-01-16 23:18:29 +0900

    Update and rename 4_Bit_Adder_Struc.v to Four_Bit_Adder_Struc.v

--- 01_Combinational_Circuits/02_Adders/Four_Bit_Adder_Struc.v
+++ 01_Combinational_Circuits/02_Adders/Four_Bit_Adder_Struc.v
@@ -2,23 +2,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Engineer:    Sungyoon Jung
 // Create Date: 2024
-// Module Name: four_bit_adder_struc.v
+// Module Name: Four_Bit_Adder_Struc.v
 // Project:     Digital Logic Design Portfolio
 //
 // Description: 
-//    Make four bit adder with structural code.
+//    Make 4-bit adder with structural code.
 //////////////////////////////////////////////////////////////////////////////////
 
-module four_bit_adder_struc(x, y, s, c4);
+module Four_Bit_Adder_Struc(x, y, s, c4);
   input [3:0] x;
   input [3:0] y;
   output [3:0] s;
   output c4;
   wire c1, c2, c3;
 
-  full_adder fulladder1(x[0], y[0], 0, s[0], c1);
-  full_adder fulladder2(x[1], y[1], c1, s[1], c2);
-  full_adder fulladder3(x[2], y[2], c2, s[2], c3);
-  full_adder fulladder4(x[3], y[3], c3, s[3], c4);
+  Full_Adder fulladder1(x[0], y[0], 0, s[0], c1);
+  Full_Adder fulladder2(x[1], y[1], c1, s[1], c2);
+  Full_Adder fulladder3(x[2], y[2], c2, s[2], c3);
+  Full_Adder fulladder4(x[3], y[3], c3, s[3], c4);
 
 endmodule
