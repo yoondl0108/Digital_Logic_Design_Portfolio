d2b70193fd15442c6add3a0625d60496d63d9b33
yoondl0108 <114382345+yoondl0108@users.noreply.github.com>
2026-01-16 23:14:23 +0900

    Update tb_Basic_Gates.v

--- 01_Combinational_Circuits/01_Basic_Gates/tb_Basic_Gates.v
+++ 01_Combinational_Circuits/01_Basic_Gates/tb_Basic_Gates.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Engineer:      Sungyoon Jung
-// Create Date:   2024
+// Create Date:   2024. 09. 11
 // Module Name:   tb_Basic_Gates.v
 // Target Module: Basic_Gates.v
 //
